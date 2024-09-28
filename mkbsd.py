9b429e88ffff563ec79a2a2ebf65f62187ece364
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-09-28 07:10:48 +0530

    Update mkbsd.py

--- mkbsd.py
+++ mkbsd.py
@@ -2,31 +2,25 @@ import os
 import time
 import aiohttp
 import asyncio
+import logging
 from pyrogram import Client, filters
 from config import *
 from database import *
 from urllib.parse import urlparse
+
 url = 'https://storage.googleapis.com/panels-api/data/20240916/media-1a-i-p~s'
 
-
-
-
+# Configure logging
 logging.basicConfig(
     filename='MKBSD-TG.log',
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
 
-
-
-
 # Create the Pyrogram client
-app = Client("SpidyPHVDL", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN,workers=100)
+app = Client("SpidyPHVDL", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, workers=10)
 
-
-
-
-
+db = connect_to_mongodb(DATABASE, "Spidydb")
 
 async def delay(ms):
     await asyncio.sleep(ms / 1000)
@@ -39,45 +33,53 @@ async def download_image(session, image_url, file_path):
             content = await response.read()
             with open(file_path, 'wb') as f:
                 f.write(content)
+        logging.info(f"🖼️ Image downloaded successfully to {file_path}")
     except Exception as e:
-        print(f"Error downloading image: {str(e)}")
+        logging.error(f"Error downloading image: {str(e)}")
 
 async def main():
     async with app:
-       try:
-         async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                if response.status != 200:
-                    raise Exception(f"⛔ Failed to fetch JSON file: {response.status}")
-                json_data = await response.json()
-                data = json_data.get('data')
-                
-                if not data:
-                    raise Exception('⛔ JSON does not have a "data" property at its root.')
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    if response.status != 200:
+                        raise Exception(f"⛔ Failed to fetch JSON file: {response.status}")
+                    json_data = await response.json()
+                    data = json_data.get('data')
 
-                download_dir = os.path.join(os.getcwd(), 'downloads')
-                if not os.path.exists(download_dir):
-                    os.makedirs(download_dir)
-                    print(f"📁 Created directory: {download_dir}")
+                    if not data:
+                        raise Exception('⛔ JSON does not have a "data" property at its root.')
 
-                file_index = 1
-                for key, subproperty in data.items():
-                    if subproperty and subproperty.get('dhd'):
-                        image_url = subproperty['dhd']
-                        print(f"🔍 Found image URL!")
-                        parsed_url = urlparse(image_url)
-                        ext = os.path.splitext(parsed_url.path)[-1] or '.jpg'
-                        filename = f"{file_index}{ext}"
-                        file_path = os.path.join(download_dir, filename)
+                    download_dir = os.path.join(os.getcwd(), 'downloads')
+                    if not os.path.exists(download_dir):
+                        os.makedirs(download_dir)
+                        logging.info(f"📁 Created directory: {download_dir}")
 
-                        await download_image(session, image_url, file_path)
-                        print(f"🖼️ Downloaded image to {file_path}")
-                        pic = await app.send_photo(Chat_ID,photo=file_path)
-                        doc = await app.send_document(Chat_ID,document=file_path)
-                        file_index += 1
-                        await delay(250)
+                    file_index = 1
+                    for key, subproperty in data.items():
+                        if subproperty and subproperty.get('dhd'):
+                            image_url = subproperty['dhd']
+                            logging.info(f"🔍 Found image URL: {image_url}")
+                            parsed_url = urlparse(image_url)
+                            ext = os.path.splitext(parsed_url.path)[-1] or '.jpg'
+                            logging.info(f"Filename : {parsed_url.path}")
+                            filename = f"{file_index}{ext}"
+                            file_path = os.path.join(download_dir, filename)
 
-       except Exception as e:
-            print(f"Error: {str(e)}")
+                            documents = find_documents(db, collection_name)
+                            downloaded_files = {doc["FILENAME"] for doc in documents}
+                            if filename not in downloaded_files:
+                                await download_image(session, image_url, file_path)
+                                pic = await app.send_photo(LOG_ID, photo=file_path, caption=parsed_url.path)
+                                doc = await app.send_document(LOG_ID, document=file_path)
+                                result = {"PIC_ID":pic.id,"DOC_ID":doc.id,"FILENAME":filename,"IMAGE_URL":image_url}
+                                file_index += 1
+                                os.remove(file_path)
+                                logging.info(f"📄 Image sent and removed from {file_path}")
+                            await delay(250)
+
+        except Exception as e:
+            logging.exception(f"Error: {str(e)}")
+
 if __name__ == "__main__":
     app.run(main())
